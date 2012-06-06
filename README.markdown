7ce686567d272cf2e115c919af1acc39d88e9856
Mark T. Holder <mtholder@gmail.com>
2012-06-06 12:46:12 -0400

    Readme and some mock up controller for phylows

---
+++ README.markdown
@@ -0,0 +1,13 @@
+Phylotastic TreeStore
+=====================
+
+
+
+
+Installation
+------------
+ 1. Download web2py from http://www.web2py.com/examples/default/download
+ 2. Download and build virtuoso from http://sourceforge.net/projects/virtuoso/files/virtuoso/
+ 3. Install the python virtuoso bindings:
+
+  pip install virtuoso    
--- controllers/phylows.py
+++ controllers/phylows.py
@@ -1,6 +1,20 @@
 # -*- coding: utf-8 -*-
 ### required - do no delete
+def _get_tree(tree_id):
+    '''
+    Need to add the db connection stuff here...
+    '''
+    return None
+
 def tree():
-    return T('hi from tree' + str(request.args))
+    if len(request.args) < 1:
+        raise HTTP(400, 'Tree ID required')
+    if len(request.args) > 1:
+        raise HTTP(400, 'Only a one tree ID can be supplied')
+    tree_id = request.args[0]
+    tree_obj = _get_tree(tree_id)
+    if tree_obj is None:
+        raise HTTP(404, 'Tree %s not found' % tree_id)
+    return T('hi from tree ' + str(request.args))
 
 
