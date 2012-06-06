a113fa6a0c6c761103d4906fc35b1732f3755951
Karen Cranston <karen.cranston@gmail.com>
2012-06-06 16:51:01 -0400

    first test implementation of query API

--- controllers/phylows.py
+++ controllers/phylows.py
@@ -4,17 +4,34 @@ def _get_tree(tree_id):
     '''
     Need to add the db connection stuff here...
     '''
-    return None
+    tree = "((whale,hippo),carlzimmer);"
+    return tree;
 
+# query URIs of the form phylows/tree/<identifier>
 def tree():
     if len(request.args) < 1:
         raise HTTP(400, 'Tree ID required')
     if len(request.args) > 1:
-        raise HTTP(400, 'Only a one tree ID can be supplied')
+        raise HTTP(400, 'Only one tree ID can be supplied')
     tree_id = request.args[0]
     tree_obj = _get_tree(tree_id)
     if tree_obj is None:
         raise HTTP(404, 'Tree %s not found' % tree_id)
-    return T('hi from tree ' + str(request.args))
+    return T(tree_obj);
 
+# query URIs of the form phylows/find/<query>
+def find():
+    # form phylows/find/tree/
+    # returns list of URIs
+    if "tree" in request.args:
+        x = request.vars.get('uri')
+        if x is None:
+            raise HTTP(400, 'no URI specified')
+        # magic involving sparql
+        # takes list of URIs and queries treestore for trees that contain those URIs
+        treedict = {"http://example.com/tree10" : { "label" : "something", "author": ""}, 
+  "http://example.com/tree34": { "label" : "something", "author": ""} }
+        return response.json(treedict)
+    else:
+        raise HTTP(400, 'query not yet implemented')
 
