9f1a8ae0a604af41284f040533047c33715df332
Mark T. Holder <mtholder@gmail.com>
2012-06-08 11:12:14 -0400

    test running against phylotastic server

--- test/test_find_then_get_tree.py
+++ test/test_find_then_get_tree.py
@@ -1,10 +1,16 @@
 #!/usr/bin/env python
-import requests
+import requests, os
 
 payload = {'taxa_uris': ['http://phylotastic.nescent.org/IDs/ID7.dog']}
 
-resp = requests.post('http://127.0.0.1:8000/PhylotasticTreeStore/phylows/find/tree',
+if os.environ.get('DEBUG_WITH_PHYLOTASTIC_SERVER'):
+    domain_name = 'http://phylotastic.nescent.org'
+else:
+    domain_name = 'http://127.0.0.1:8000'
+
+resp = requests.post(domain_name + '/PhylotasticTreeStore/phylows/find/tree',
                      params=payload)
+print resp.url
 problem =  resp.raise_for_status()
 if problem is None:
     print resp.text
@@ -13,5 +19,5 @@ else:
 results = resp.json
 for tree_uri in results.keys():
     print tree_uri
-    tree_resp = requests.get('http://127.0.0.1:8000/PhylotasticTreeStore/phylows/tree', params={'tree_uri':tree_uri})
+    tree_resp = requests.get(domain_name + '/PhylotasticTreeStore/phylows/tree', params={'tree_uri':tree_uri})
     print "Result:", tree_resp.content
