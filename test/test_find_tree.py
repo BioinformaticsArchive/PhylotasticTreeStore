b3587acfa0be8809ac88349dc89d9c150522a8e9
Mark T. Holder <mtholder@gmail.com>
2012-06-08 11:33:51 -0400

    test mod to call remote server

--- test/test_find_tree.py
+++ test/test_find_tree.py
@@ -1,12 +1,17 @@
 #!/usr/bin/env python
 import requests
 import json
-
+import os
 payload = {'taxa_uris': ['http://phylotastic.nescent.org/IDs/ID7.dog',
                    'http://phylotastic.nescent.org/IDs/ID1.bear']}
 
-resp = requests.post('http://127.0.0.1:8000/PhylotasticTreeStore/phylows/find/tree',
-                     data=json.dumps(payload))
+if os.environ.get('DEBUG_WITH_PHYLOTASTIC_SERVER'):
+    domain_name = 'http://phylotastic.nescent.org'
+else:
+    domain_name = 'http://127.0.0.1:8000'
+data = json.dumps(payload)
+print data
+resp = requests.post(domain_name + 'PhylotasticTreeStore/phylows/find/tree', data=data)
 print resp.url
 problem =  resp.raise_for_status()
 if problem is None:
